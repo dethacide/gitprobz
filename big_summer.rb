c8e19ced298dc33d2841e09abd4eecf911fa1482
Jacquelyn Kominik <jkominik@gmail.com>
2017-10-26 10:33:07 -0400

    Fizzbuz

---
+++ big_summer.rb
@@ -0,0 +1,16 @@
+def big_summer
+	ray gun +parents fighering
+	return summer
+end
+
+def fizzbuzz(num)
+	if num % 3== 0 && num % 5 ==0
+		return "Fizzbuzz"
+	elsif num % 3== 0
+		return "fizz"
+	elsif num % 5 ==0
+		return "buzz"
+	else
+		return num
+	end
+end
